917209e6c7578afa52a4542f93b6b92b97032643
Damien Archambault--Roy <damien.archambaultroy@gmail.com>
2023-08-15 16:09:24 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import './assets/css/style.css';
+import 'reveal.js/dist/reveal.css';
+import 'reveal.js/dist/theme/moon.css';
+import Reveal from 'reveal.js';
+
+const deck = new Reveal();
+deck.initialize({minScale: 0.2});
